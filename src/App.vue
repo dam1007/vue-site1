b5d6a2e448cc466351e94aa50cb23433038b0a59
Dami <dam1007@naver.com>
2022-07-18 12:31:54 +0900

    220718
    뷰 사이트1

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template> 
+  <HeaderSection />
+  <SliderSection />
+  <ImageSection skill="section"/>
+  <ImgtextSection />
+  <CardSection />
+</template>
+
+<script>
+  import HeaderSection from './components/HeaderSection.vue'
+  import SliderSection from './components/SliderSection.vue'
+  import ImageSection from './components/ImageSection.vue'
+  import ImgtextSection from './components/ImgtextSection.vue'
+  import CardSection from './components/CardSection.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderSection,
+      SliderSection,
+      ImageSection,
+      ImgtextSection,
+      CardSection,
+    }
+  }
+
+</script>
+<style>
+  @import "./App.css";
+</style>
